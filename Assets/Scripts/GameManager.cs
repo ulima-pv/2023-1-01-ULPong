bc962a3d70cabeaedafce59d051e0f4b589a2904
ProfL3502 <ProfL3502@lab.aca>
2023-04-21 17:48:54 -0500

    Pong completo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,10 +9,21 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI TextScore1;
     public TextMeshProUGUI TextScore2;
     public BallMovement ball;
+    public PaddleMovement paddle1;
+    public PaddleMovement paddle2;
 
     private void Start()
     {
         ball.OnGoal += OnGoalDelegate;
+        StartGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartGame();
+        }
     }
 
     private void OnGoalDelegate(object sender, EventArgs e)
@@ -30,5 +41,24 @@ public class GameManager : MonoBehaviour
             int puntaje = int.Parse(TextScore2.text);
             TextScore2.text = (puntaje + 1).ToString();
         }
+        StopGame();
+    }
+    private void StartGame()
+    {
+        TextScore1.gameObject.SetActive(false);
+        TextScore2.gameObject.SetActive(false);
+        ball.Run();
+        paddle1.Run();
+        paddle2.Run();
+    }
+
+    private void StopGame()
+    {
+        TextScore1.gameObject.SetActive(true);
+        TextScore2.gameObject.SetActive(true);
+        ball.Stop();
+        paddle1.Stop();
+        paddle2.Stop();
+        
     }
 }
