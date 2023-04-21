d65571663acaac39006a5cc2dd79ec936ea1790b
ProfL3502 <ProfL3502@lab.aca>
2023-04-21 16:13:25 -0500

    Gameplay terminado

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -10,17 +10,31 @@ public class BallMovement : MonoBehaviour
     private void Start()
     {
         rb = transform.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(Speed.x, Speed.y);
+        //rb.velocity = new Vector2(Speed.x, Speed.y);
     }
 
     private void Update()
     {
         //transform.position += Vector3.right * Speed * Time.deltaTime;
-        
+        rb.velocity = new Vector2(Speed.x, Speed.y);
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        Speed *= -1f;
+        if (col.transform.CompareTag("Wall"))
+        {
+            Speed.y *= -1f;
+        }else 
+        {
+            Speed.y = Random.Range(-5f, 5f);
+            Speed.x *= -1f;
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Gol!");
+        transform.position = new Vector3(0f, 0f ,0f);
+        
     }
 }
